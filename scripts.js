176c0c4bd2d8f5da40f733dd3f75cedb82e16fa7
Start Coding Student 2 <session2@START-Coding-MBP-13.local>
2016-02-26 17:28:14 -0700

    transferred files over from extension I was working on.

---
+++ scripts.js
@@ -0,0 +1,32 @@
+function changeBackgroundRed(red) {
+    document.body.style.background = "#ff0000";
+}
+
+document.getElementById('red').onclick = changeBackgroundRed();
+
+function changeBackgroundBlue (blue) {
+    document.body.style.background = "#0000ff";
+}
+
+document.getElementById('blue').onclick = changeBackgroundBlue();
+
+function changeBackgroundGreen(green) {
+    document.body.style.background = "#329932";
+}
+
+document.getElementById('green').onclick = changeBackgroundGreen();
+
+function changeBackgroundOrange(orange) {
+    document.body.style.background = "#ffae19";
+}
+
+document.getElementById('orange').onclick = changeBackgroundOrange();
+
+function changeBackgroundPink(pink) {
+    document.body.style.background = "#ffc6d0";
+}
+
+document.getElementById('pink').onclick = changeBackgroundPink();
+
+console.log('hfjdsjf;adfdkls;fas');
+
